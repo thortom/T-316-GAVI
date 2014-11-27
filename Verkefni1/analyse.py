b4162030c076eb20f64c3e4dfa154d6d9d56b598
Thor Tomasarson <thortomasarson@gmail.com>
2014-11-27 13:13:12 +0000

    added the Verkefni1 folder

---
+++ Verkefni1/analyse.py
@@ -0,0 +1,11 @@
+import csv as csv
+import numpy as np
+
+fileName = "Gistingar.xlsx"
+
+def getData(fileName):
+	# TODO: read this file
+	return title, data
+
+if __name__ == '__main__':
+	title, data = getData(fileName);
