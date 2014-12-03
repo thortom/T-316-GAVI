4dd8df24a4ef0956be3f23c9cbeeef1f87c94173
Arnar <arnarh12@ru.is>
2014-12-03 13:45:00 +0000

    statistics class is now has kickass plotter

--- Verkefni1/statistics.py
+++ Verkefni1/statistics.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 class statistics:
     def __init__(self, dfIslendingarGesta, dfUtlendingarGesta, dfIslendingarGisti, dfUtlendingarGisti):
         self.dfIGe = dfIslendingarGesta
@@ -8,6 +9,9 @@ class statistics:
 
         #print(self.getAvIncr(self.dfUGi))
         #print(self.getAvIncrMonth(self.dfUGi,3))
+        #print(self.dfUGi)
+        #self.plotLine(self.getMonth(self.dfUGi,9)) 
+        self.plotAll(self.dfUGi,months=[8,9,10,11])
     def getAvIncr(self,df):
         #Prints column 0, row 1
         #Prints year 1998, Februar
@@ -30,3 +34,40 @@ class statistics:
     def getAvIncrMonth(self,df,month):
         dfAvIncr = self.getAvIncr(df)
         return(dfAvIncr[0][month])
+
+    def getMonth(self,df,month,years = None):
+        if years == None:
+            years = list(range(len(df.columns)))
+            #print(years)
+        return(df[years][month])
+
+    def plotLine(self,df):
+        fig = plt.figure("myFig!")
+        plt.plot(df)
+        plt.show()
+
+    def plotAll(self,df,months=None,years=None):
+        if months == None:
+            months = list(range(len(df.index)))
+        if years == None:
+            years = list(range(len(df.columns)-1))
+        
+        monthName={0:'Jan', 1:'Feb', 2:'Mars', 3:'April', 4:'May', 5:'Juni', 6:'July', 7:'Agust', 8:'September', 9:'Oktober', 10:'November', 11:'Desember'}
+        index=[]
+        for i in months:
+            index.append(monthName[i])
+
+        #Sniðugt til að sækja rétt gögn
+        for year in years:
+            pass
+            #print(df[year][months])
+        for month in months:
+            pass
+            #print(df.T[month][years])
+
+        fig = plt.figure('my plot')
+        for year in years:
+            toPlot = df[year][months]
+            toPlot.index=index
+            toPlot.plot()
+        plt.show()
