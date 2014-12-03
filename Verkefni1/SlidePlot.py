319b2258c0d09e9d14ba38268ab24e22f7788208
Thor Tomasarson <thortomasarson@gmail.com>
2014-12-03 09:20:38 +0000

    Taka til í SliderPlot.py

--- Verkefni1/SlidePlot.py
+++ Verkefni1/SlidePlot.py
@@ -11,97 +11,79 @@ class SlidePlot:
     monthPlayed = {1999: 10-1, 2000: 10-1, 2001: 10-1, 2002: 10-1, 2003: 10-1, 2004: 10-1,\
                     2005: 10-1, 2006: 10-1, 2006: 10-1, 2007: 10-1, 2008: 10-1, 2009: 10-1,\
                     2010: 10-1, 2011: 10-1, 2012: 11-1, 2013: 11-1, 2014: 11-1}                     # 'Oktober' == 10-1
+    xticklabels = ['Jan','Feb','Mars','Apríl','Maí','Júní','Júlí','Ágúst','Sept','Okt','Nóv','Des']
 
     def __init__(self, data, title):
         self.data = data
-        self.yearDict = dict(zip(range(1998,2015), range(0, 2015-1998)))
-        self.year0 = 1998
         self.title = title
+        self.yearMin = 1998
+        self.yearMax = 2014
 
+        self.yearDict = dict(zip(range(self.yearMin, self.yearMax+1), range(0, (self.yearMax+1) - self.yearMin)))
         self.yMaxValue = np.amax(self.data) + np.amax(self.data)*0.1
 
         self.fig = plt.figure()
-        self.ax1 = plt.subplot(2, 1, 1)
+        self.barAxis = plt.subplot(2, 1, 1)
         plt.subplots_adjust(left=0.1, bottom=-0.40)
-
         
-        self.x = np.arange(len(self.data[self.yearDict[self.year0]]))
-        barPlot = self.ax1.bar(self.x, self.data[self.yearDict[self.year0]])
-        self.colorBar(self.ax1, self.year0)
-        self.ax1.set_ybound(0, self.yMaxValue)
-        plt.xticks(self.x + 0.5)
-        self.ax1.set_xticklabels(['Jan','Feb','Mars','Apríl','Maí','Júní','Júlí','Ágúst','Sept','Okt','Nóv','Des'])
-        plt.title(self.title + ", " + str(self.year0))
+        self.x = np.arange(len(self.data[self.yearDict[self.yearMin]]))
+        self.setDataOnPlot(self.barAxis, self.yearMin)
 
+        # Create the slider bar
         axcolor = 'lightgoldenrodyellow'
-        axYear = plt.axes([0.18, 0.2, 0.65, 0.03], axisbg=axcolor)
-        self.slideYear = Slider(axYear, 'Year', 1998, 2014, valinit=1998, valfmt='%0.0f')
+        yearAxis = plt.axes([0.18, 0.2, 0.65, 0.03], axisbg=axcolor)
+        self.slideYear = Slider(yearAxis, 'Year', 1998, 2014, valinit=1998, valfmt='%0.0f')
 
-        self.slideYear.on_changed(self.update)
-        b1 = self.ax1.bar([0,0,0],[0,0,0],color = "black", width = 0.4, label ="Bar 2", align = "center")
-        b2 = self.ax1.bar([0,0,0],[0,0,0],color = "blue", width = 0.4, label ="Bar 2", align = "center")
-        self.ax1.legend([b1,b2], [' Airwaves','!Airwaves'])
+        self.slideYear.on_changed(self.update)                                      # on_changed() method checks for changes in the slider
 
-
+        # Create the Next and Previous buttons
         self.axprev = plt.axes([0.4, 0.05, 0.1, 0.075])
         self.axnext = plt.axes([0.51, 0.05, 0.1, 0.075])
         self.bnext = Button(self.axnext, 'Next')
         self.bnext.on_clicked(self.next)
         self.bprev = Button(self.axprev, 'Previous')
 
-        self.bprev.on_clicked(self.prev)
+        self.bprev.on_clicked(self.prev)                                            # on_clicked() method checks for button clicks
         
         plt.show()
+
+    def setDataOnPlot(self, axis, year):
+        axis.cla()                                                                  # Clear axis
+        axis.bar(self.x, self.data[self.yearDict[year]])
+
+        axis.set_title(self.title + ", " + str(year))
+        axis.set_xticks(self.x + 0.5)
+        axis.set_xticklabels(self.xticklabels)
+
+        self.colorBar(axis, year)
+        b1 = axis.bar([0,0,0],[0,0,0],color = "black", width = 0.4, label ="Bar 2", align = "center")
+        b2 = axis.bar([0,0,0],[0,0,0],color = "blue", width = 0.4, label ="Bar 2", align = "center")
+        axis.legend([b1,b2], [' Airwaves','!Airwaves'])
+
+        axis.set_ybound(0, self.yMaxValue)
 
     def update(self, val):
         self.slideYear.val = int(round(self.slideYear.val))
         year = self.slideYear.val
-        self.ax1.cla()                                                                  # Clear axis
-        barPlot = self.ax1.bar(self.x, self.data[self.yearDict[year]])
-        self.ax1.set_ybound(0, self.yMaxValue)
-        self.ax1.set_title(self.title + ", " + str(year))
-        self.ax1.set_xticks(self.x + 0.5)
-        self.ax1.set_xticklabels(['Jan','Feb','Mars','Apríl','Maí','Júní','Júlí','Ágúst','Sept','Okt','Nóv','Des'])
-        self.colorBar(self.ax1, year)
-        b1 = self.ax1.bar([0,0,0],[0,0,0],color = "black", width = 0.4, label ="Bar 2", align = "center")
-        b2 = self.ax1.bar([0,0,0],[0,0,0],color = "blue", width = 0.4, label ="Bar 2", align = "center")
-        self.ax1.legend([b1,b2], [' Airwaves','!Airwaves'])
 
-        self.ax1.set_ybound(0, self.yMaxValue)
-
+        self.setDataOnPlot(self.barAxis, year)
         self.fig.show()
         plt.draw()
 
     def next(self, val):
-        if self.slideYear.val != 2014:
-            self.slideYear.val = int(round(self.slideYear.val)+1)
-            year = self.slideYear.val
-            self.ax1.cla()                                                               # Clear axis
-            barPlot = self.ax1.bar(self.x, self.data[self.yearDict[year]])
-            self.ax1.set_ybound(0, self.yMaxValue)
-            self.ax1.set_title(self.title + ", " + str(year))
-            self.ax1.set_xticks(self.x + 0.5)
-            self.ax1.set_xticklabels(['Jan','Feb','Mars','Apríl','Maí','Júní','Júlí','Ágúst','Sept','Okt','Nóv','Des'])
-            self.slideYear.set_val(year)
-            self.fig.show()
-            plt.draw()
+        if self.slideYear.val < self.yearMax:
+            self.slideYear.set_val(self.slideYear.val + 1)                          # .set_val calls the update() function for the 
+        else:
+            self.slideYear.set_val(self.yearMax)
 
     def prev(self,val):
-        if self.slideYear.val != 1998:
-            self.slideYear.val = int(round(self.slideYear.val)-1)
-            year = self.slideYear.val
-            self.ax1.cla()                                                               # Clear axis
-            barPlot = self.ax1.bar(self.x, self.data[self.yearDict[year]])
-            self.ax1.set_ybound(0, self.yMaxValue)
-            self.ax1.set_title(self.title + ", " + str(year))
-            self.ax1.set_xticks(self.x + 0.5)
-            self.ax1.set_xticklabels(['Jan','Feb','Mars','Apríl','Maí','Júní','Júlí','Ágúst','Sept','Okt','Nóv','Des'])
-            self.slideYear.set_val(year)
-            self.fig.show()
-            plt.draw()
+        if self.slideYear.val > self.yearMin:
+            self.slideYear.set_val(self.slideYear.val - 1)                          # .set_val calls the update() function for the slider
+        else:
+            self.slideYear.set_val(self.yearMin)
 
     def colorBar(self, ax, year):
-        month = self.monthPlayed.get(year, 11-1)                                 # Default is 'November' == 11-1
+        month = self.monthPlayed.get(year, 11-1)                                    # Default is 'November' == 11-1
         ax.bar(month, self.data[self.yearDict[year]][month], color='black')
 
     
