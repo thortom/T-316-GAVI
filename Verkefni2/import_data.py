2a6bc6946f88d3f7fccba555199d09c27618fd5e
Arnar <arnarh12@ru.is>
2014-12-06 18:26:22 +0000

    function to read the data files

--- Verkefni2/import_data.py
+++ Verkefni2/import_data.py
@@ -5,17 +5,25 @@ import pandas as pd
 
 class import_data():
     def __init__(self):
-        self.findData()
+        dataFiles = self.findData()
+        for file in dataFiles:
+            self.readDat(file)
         pass
 
     def findData(self):
         #Finds .dat files in the folder /data
+        dataFiles = []
         for file in os.listdir(os.getcwd()+"\data"):
             if file.endswith(".dat"):
-                print(file)
-        pass
+                dataFiles.append(file)
 
-    def readDat(self):
-        pass
+        return dataFiles
+
+    def readDat(self,file):
+        print(file)
+        if file == "movies.dat":
+            df = pd.read_csv('data/'+file,delimiter='::', header=None,engine='python')
+            #print(df)
+        return df
 
 
