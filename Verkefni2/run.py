65a7fa00b69bd1a8e4c268dc6ccf62a901040014
Arnar <addi1204@gmail.com>
2014-12-08 13:28:40 +0000

    removed test function from runner

--- Verkefni2/run.py
+++ Verkefni2/run.py
@@ -11,20 +11,5 @@ if __name__ == '__main__':
     if mydb.missingData():
     	data = import_data()
     	mydb.insertTable(data)
-
-    ##########################
-    # Testing
-    con = mydb.getConnection()
-    cur = con.cursor()
-    cur.execute("SELECT tag FROM tags")
-    count = 0
-    while True:
-        row = cur.fetchone()
-            
-        if row == None or count == 10:
-            break
-        print(row)
-        count += 1
-    ##########################
-
+        
     window = theUI.loadUI(mydb)
