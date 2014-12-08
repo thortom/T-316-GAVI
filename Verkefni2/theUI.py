bac82de2d0c87f1758c81af61e62055537afb74f
Arnar <addi1204@gmail.com>
2014-12-08 17:48:48 +0000

    testing with changing dropdown menus

--- Verkefni2/theUI.py
+++ Verkefni2/theUI.py
@@ -23,9 +23,19 @@ class Main(QtGui.QMainWindow):
         self.mydb = mydb
 
         catagories = ["Pick a genre", "Action", "Adventure",    "Animation",    "Children's",   "Comedy",   "Crime",    "Documentary",  "Drama",    "Fantasy",  "Film-Noir",    "Horror",   "Musical",  "Mystery",  "Romance",  "Sci-Fi",   "Thriller", "War",  "Western"]
-        dropdowns = [self.ui.Genre_1_dropdown,self.ui.Genre_2_dropdown,self.ui.Genre_3_dropdown]
-        for dropdown in dropdowns:
+        self.activeCatagories = []
+        self.dropdowns = [self.ui.Genre_1_dropdown,self.ui.Genre_2_dropdown,self.ui.Genre_3_dropdown]
+        for dropdown in self.dropdowns:
             self.loadCatagories(dropdown,catagories)
+            dropdown.currentIndexChanged.connect(self.updateCatagories)
+
+    def updateCatagories(self):
+        print('updated')
+        for dropdown in self.dropdowns:
+            if dropdown.currentIndex() != 0:
+                print(dropdown.currentIndex())
+                self.activeCatagories.append(dropdown.itemText(dropdown.currentIndex()))
+                print('activeCatagories:',self.activeCatagories)
 
     def loadCatagories(self, button, catagories):
         for catagorie in catagories:
