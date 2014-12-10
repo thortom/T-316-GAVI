f0dc25cd907e37b7b13c5bd7c8686174d6248631
Bubbi12 <hjorleifur02@gmail.com>
2014-12-10 20:11:14 +0000

    Buzz Fizz Fix GUI, add a butler to the service

--- Verkefni2/theUI.py
+++ Verkefni2/theUI.py
@@ -1,4 +1,4 @@
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 from ui.window import Ui_MainWindow
 import sys
 from google import search
@@ -22,6 +22,11 @@ class Main(QtGui.QMainWindow):
         self.ui.Gen_ran_btn.clicked.connect(self.Gen_ran_btn_Clicked)
         self.ui.Gen_Random_Movie_btn.clicked.connect(self.Gen_Random_Movie_btn_Clicked)
         self.mydb = mydb
+        cb = self.ui.checkBox
+        cb.toggle()
+        cb.stateChanged.connect(self.checkBox)
+        self.DATRUTH = True
+
 
 
         catagories = ["Pick a genre", "Action", "Adventure",    "Animation",    "Children",   "Comedy",   "Crime",    "Documentary",  "Drama",    "Fantasy",  "Film-Noir",    "Horror",   "Musical",  "Mystery",  "Romance",  "Sci-Fi",   "Thriller", "War",  "Western"]
@@ -31,6 +36,13 @@ class Main(QtGui.QMainWindow):
             for catagorie in catagories:
                 dropdown.addItem(catagorie)
 
+
+    def checkBox(self,state):
+        cb = self.checkBox
+        if state == QtCore.Qt.Checked:
+            self.DATRUTH = True
+        else:
+            self.DATRUTH = False
 
     def genTopX_Clicked(self):
         toplist = str(self.ui.Top_x_dropdown.currentText())
@@ -120,7 +132,8 @@ class Main(QtGui.QMainWindow):
                 self.ui.textBrowser.append('No imdb link was found for %s' % RandMovie)
             else:
                 self.ui.textBrowser.append('Imdb link for %s is %s ' %(RandMovie,imdbList[0]))
-                webbrowser.open(imdbList[0])
+                if self.DATRUTH:
+                    webbrowser.open(imdbList[0])
 
 
 
