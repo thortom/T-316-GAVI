0bab4bf23a4ff5e1c0b6dbbe531f7b9b8ddf4738
Arnar <addi1204@gmail.com>
2014-12-14 16:54:42 +0000

    refactored shit

--- Verkefni3/theUI.py
+++ Verkefni3/theUI.py
@@ -24,41 +24,45 @@ class Main(QtGui.QMainWindow):
         self.Graph = self.ui.graphicsView
         self.mydb = mydb
         self.curr = self.mydb.cursor
+        self.ui.ClearPlot.clicked.connect(self.ClearPlot_clicked)
+        self.ui.Plot.clicked.connect(self.Plot_clicked)
         self.ListCol = []
 
+        self.list = self.ui.listView
+        self.initializeDropdowns()
+        self.setCheckBoxes()
+        self.setInfo()
 
-
+    def initializeDropdowns(self):
         self.curr.execute("SELECT Distinct country from world_info")
         row = self.curr.fetchall()
         Country = []
         for i in row:
             Country.append(i[0])
         Country.sort()
-        Columns = []
-        self.curr.execute("Select * from world_info LIMIT 0")
-        for idx, col in enumerate(self.curr.description):
-            Columns.append(col[0])
 
-        self.ui.ClearPlot.clicked.connect(self.ClearPlot_clicked)
-        self.ui.Plot.clicked.connect(self.Plot_clicked)
-        catagories = Country #["Pick Country","Iceland", "Denmark", "Sweden", "Norway", "Penis"]
+        catagories = Country
         self.dropdowns = [self.ui.CountryBox]
 
         for dropdown in self.dropdowns:
             for catagorie in catagories:
                 dropdown.addItem(catagorie)
 
-        # self.ui.checkBox.clicked.connect(self.chb1_clicked)
-        # self.ui.checkBox_2.clicked.connect(self.chb2_clicked)
-        # self.ui.checkBox_3.clicked.connect(self.chb3_clicked)
+    def findColumns(self):
+        columns = []
+        self.curr.execute("Select * from world_info LIMIT 0")
+        for idx, col in enumerate(self.curr.description):
+            print(col[0])
+            if col[0] != 'country' and col[0] != 'year':
+                self.curr.execute("Select count(%s) from world_info where country = '%s'" %(col[0],self.ui.CountryBox.currentText()))
+                rows = self.curr.fetchall()
+                if rows[0][0] != 0:
+                    columns.append(col[0])
+        return columns
 
-        #app = QtGui.QApplication(sys.argv) 
-        #list = QtGui.QListView()
-        self.list = self.ui.listView
-        #list.setWindowTitle('Example List')
-        #list.setMinimumSize(600, 400)
+    def setCheckBoxes(self):
         self.model = QtGui.QStandardItemModel(self.list)
-        self.foods = Columns
+        self.foods = self.findColumns()
         for food in self.foods:
             self.item = QtGui.QStandardItem(food)
             self.item.setCheckable(True)
@@ -68,13 +72,13 @@ class Main(QtGui.QMainWindow):
 
         self.model.itemChanged.connect(self.CheckBox_changed)
 
-        
-        self.Graph.setLabel('left', 'Value')
-        self.Graph.setLabel('bottom', 'Years')
-        self.Graph.setXRange(1960, 2020)
+
+    def setInfo(self,left='Value',bottom='Years',x1=1930,x2=2020):
+        self.Graph.setLabel('left', left)
+        self.Graph.setLabel('bottom', bottom)
+        self.Graph.setXRange(x1, x2)
         #self.Graph.setYRange(0, 100)
 
-        #app.exec_()
 
     def chb1_clicked(self):
         print('clickkk')
@@ -160,9 +164,9 @@ class Main(QtGui.QMainWindow):
         if state == 'CHECKED':
             self.Graph.plot([0,2,4,6,10],[5,1,7,9,7], pen='b')
 
-        rect = QtGui.QGraphicsRectItem(QtCore.QRectF(1990, 10, 20, 80))
-        rect.setPen(QtGui.QPen(QtGui.QColor(100, 200, 100)))
-        self.Graph.addItem(rect,'green rect')
+        #rect = QtGui.QGraphicsRectItem(QtCore.QRectF(1990, 10, 20, 80))
+        #rect.setPen(QtGui.QPen(QtGui.QColor(100, 200, 100)))
+        #self.Graph.addItem(rect,'green rect')
 
         # state = ['UNCHECKED', 'TRISTATE',  'CHECKED'][self.model.item(0).checkState()]
         # print(self.model.item(0))
