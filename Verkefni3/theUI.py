d3fce7b9c26f7fc246d80c76002dfaac5fdeae98
Bubbi12 <hjorleifur02@gmail.com>
2014-12-14 11:55:18 +0000

    Fixing the QT ass

--- Verkefni3/theUI.py
+++ Verkefni3/theUI.py
@@ -16,26 +16,40 @@ class Main(QtGui.QMainWindow):
         QtGui.QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.Graph = self.ui.graphicsView
 
-        self.ui.graphicsView.plot([0,1,2,3,4,5], [3,2,1,4,5,7], pen='r')
+        #self.ui.graphicsView.plot([0,1,2,3,4,5], [3,2,1,4,5,7], pen='r')
 
-        self.ui.checkBox.clicked.connect(self.chb1_clicked)
-        self.ui.checkBox_2.clicked.connect(self.chb2_clicked)
-        self.ui.checkBox_3.clicked.connect(self.chb3_clicked)
+
+        self.ui.ClearPlot.clicked.connect(self.ClearPlot_clicked)
+        self.ui.Plot.clicked.connect(self.Plot_clicked)
+        catagories = ["Pick Country","Iceland", "Denmark", "Sweden", "Norway", "Penis"]
+        self.dropdowns = [self.ui.CountryBox]
+
+        for dropdown in self.dropdowns:
+            for catagorie in catagories:
+                dropdown.addItem(catagorie)
+
+        # self.ui.checkBox.clicked.connect(self.chb1_clicked)
+        # self.ui.checkBox_2.clicked.connect(self.chb2_clicked)
+        # self.ui.checkBox_3.clicked.connect(self.chb3_clicked)
 
         #app = QtGui.QApplication(sys.argv) 
         #list = QtGui.QListView()
-        list = self.ui.listView
+        self.list = self.ui.listView
         #list.setWindowTitle('Example List')
         #list.setMinimumSize(600, 400)
-        model = QtGui.QStandardItemModel(list)
-        foods = ['a','b','c']
-        for food in foods:
-            item = QtGui.QStandardItem(food)
-            item.setCheckable(True)
-            model.appendRow(item)
-        list.setModel(model)
-        #list.show()
+        self.model = QtGui.QStandardItemModel(self.list)
+        self.foods = ['GDP','School Enrollment','c']
+        for food in self.foods:
+            self.item = QtGui.QStandardItem(food)
+            self.item.setCheckable(True)
+            self.model.appendRow(self.item)
+        self.list.setModel(self.model)
+        self.list.show()
+
+        self.model.itemChanged.connect(self.CheckBox_changed)
+
         #app.exec_()
 
     def chb1_clicked(self):
@@ -49,3 +63,32 @@ class Main(QtGui.QMainWindow):
     def chb3_clicked(self):
         print('clickkk')
         print(self.ui.checkBox_3.checkState())
+
+    def CheckBox_changed(self, item):
+
+        i = 0
+
+        while self.model.item(i):
+            state = ['UNCHECKED', 'TRISTATE',  'CHECKED'][self.model.item(i).checkState()]
+            print(self.model.item(i).text(), state)
+            i += 1
+
+    def ClearPlot_clicked(self):
+        self.Graph.clear()
+
+    def Plot_clicked(self):
+
+        Country = str(self.ui.CountryBox.currentText())
+
+
+        
+
+        state = ['UNCHECKED', 'TRISTATE',  'CHECKED'][self.model.item(1).checkState()]
+        print(self.model.item(1))
+        if state == 'CHECKED':
+            self.Graph.plot([0,1,2,3,4,5], [3,2,1,4,5,7], pen='r')
+        
+        state = ['UNCHECKED', 'TRISTATE',  'CHECKED'][self.model.item(0).checkState()]
+        print(self.model.item(0))
+        if state == 'CHECKED':
+            self.Graph.plot([0,2,4,6,10],[5,1,7,9,7], pen='b')
--- Verkefni3/ui/window.py
+++ Verkefni3/ui/window.py
@@ -2,136 +2,7 @@
 
 # Form implementation generated from reading ui file 'window.ui'
 #
-# Created: Sat Dec 13 14:57:07 2014
-#      by: PyQt4 UI code generator 4.11.3
-#
-# WARNING! All changes made in this file will be lost!
-
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtGui.QWidget(MainWindow)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.label = QtGui.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(300, 180, 71, 16))
-        self.label.setObjectName(_fromUtf8("label"))
-        self.tabWidget = QtGui.QTabWidget(self.centralwidget)
-        self.tabWidget.setGeometry(QtCore.QRect(90, 250, 127, 80))
-        self.tabWidget.setObjectName(_fromUtf8("tabWidget"))
-        self.tab = QtGui.QWidget()
-        self.tab.setObjectName(_fromUtf8("tab"))
-        self.tabWidget.addTab(self.tab, _fromUtf8(""))
-        self.tab_2 = QtGui.QWidget()
-        self.tab_2.setObjectName(_fromUtf8("tab_2"))
-        self.tabWidget.addTab(self.tab_2, _fromUtf8(""))
-        self.graphicsView = PlotWidget(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(370, 290, 256, 192))
-        self.graphicsView.setObjectName(_fromUtf8("graphicsView"))
-        self.frame = QtGui.QFrame(self.centralwidget)
-        self.frame.setGeometry(QtCore.QRect(140, 420, 120, 80))
-        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
-        self.frame.setFrameShadow(QtGui.QFrame.Raised)
-        self.frame.setObjectName(_fromUtf8("frame"))
-        self.graphicsView_2 = QtGui.QGraphicsView(self.centralwidget)
-        self.graphicsView_2.setGeometry(QtCore.QRect(390, 70, 256, 192))
-        self.graphicsView_2.setObjectName(_fromUtf8("graphicsView_2"))
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-        self.menubar.setObjectName(_fromUtf8("menubar"))
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtGui.QStatusBar(MainWindow)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        self.tabWidget.setCurrentIndex(0)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("MainWindow", "Verkefni 3", None))
-        self.label.setText(_translate("MainWindow", "Wazup bitches", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Tab 1", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Tab 2", None))
-
-from pyqtgraph import PlotWidget
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'window.ui'
-#
-# Created: Sat Dec 13 15:10:22 2014
-#      by: PyQt4 UI code generator 4.11.3
-#
-# WARNING! All changes made in this file will be lost!
-
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtGui.QWidget(MainWindow)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.graphicsView = PlotWidget(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(200, 60, 561, 271))
-        self.graphicsView.setObjectName(_fromUtf8("graphicsView"))
-        self.frame = QtGui.QFrame(self.centralwidget)
-        self.frame.setGeometry(QtCore.QRect(140, 420, 120, 80))
-        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
-        self.frame.setFrameShadow(QtGui.QFrame.Raised)
-        self.frame.setObjectName(_fromUtf8("frame"))
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-        self.menubar.setObjectName(_fromUtf8("menubar"))
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtGui.QStatusBar(MainWindow)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("MainWindow", "Verkefni 3", None))
-
-from pyqtgraph import PlotWidget
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'window.ui'
-#
-# Created: Sat Dec 13 15:18:43 2014
+# Created: Sun Dec 14 11:53:21 2014
 #      by: PyQt4 UI code generator 4.11.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -159,199 +30,27 @@ class Ui_MainWindow(object):
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.graphicsView = PlotWidget(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(200, 60, 731, 311))
+        self.graphicsView.setGeometry(QtCore.QRect(200, 60, 731, 381))
         self.graphicsView.setObjectName(_fromUtf8("graphicsView"))
         self.verticalLayoutWidget = QtGui.QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(50, 160, 111, 111))
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(20, 60, 160, 381))
         self.verticalLayoutWidget.setObjectName(_fromUtf8("verticalLayoutWidget"))
         self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setMargin(0)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.checkBox = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox.setObjectName(_fromUtf8("checkBox"))
-        self.verticalLayout.addWidget(self.checkBox)
-        self.checkBox_2 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox_2.setObjectName(_fromUtf8("checkBox_2"))
-        self.verticalLayout.addWidget(self.checkBox_2)
-        self.checkBox_3 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox_3.setObjectName(_fromUtf8("checkBox_3"))
-        self.verticalLayout.addWidget(self.checkBox_3)
-        self.checkBox_4 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox_4.setObjectName(_fromUtf8("checkBox_4"))
-        self.verticalLayout.addWidget(self.checkBox_4)
-        self.checkBox_5 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox_5.setObjectName(_fromUtf8("checkBox_5"))
-        self.verticalLayout.addWidget(self.checkBox_5)
-        self.comboBox = QtGui.QComboBox(self.centralwidget)
-        self.comboBox.setGeometry(QtCore.QRect(60, 130, 91, 21))
-        self.comboBox.setObjectName(_fromUtf8("comboBox"))
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 992, 21))
-        self.menubar.setObjectName(_fromUtf8("menubar"))
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtGui.QStatusBar(MainWindow)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("MainWindow", "Verkefni 3", None))
-        self.checkBox.setText(_translate("MainWindow", "CheckBox", None))
-        self.checkBox_2.setText(_translate("MainWindow", "CheckBox", None))
-        self.checkBox_3.setText(_translate("MainWindow", "CheckBox", None))
-        self.checkBox_4.setText(_translate("MainWindow", "CheckBox", None))
-        self.checkBox_5.setText(_translate("MainWindow", "CheckBox", None))
-
-from pyqtgraph import PlotWidget
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'window.ui'
-#
-# Created: Sat Dec 13 16:45:33 2014
-#      by: PyQt4 UI code generator 4.11.3
-#
-# WARNING! All changes made in this file will be lost!
-
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(992, 655)
-        self.centralwidget = QtGui.QWidget(MainWindow)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.graphicsView = PlotWidget(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(200, 60, 731, 311))
-        self.graphicsView.setObjectName(_fromUtf8("graphicsView"))
-        self.verticalLayoutWidget = QtGui.QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(50, 160, 111, 111))
-        self.verticalLayoutWidget.setObjectName(_fromUtf8("verticalLayoutWidget"))
-        self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setMargin(0)
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.checkBox = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox.setObjectName(_fromUtf8("checkBox"))
-        self.verticalLayout.addWidget(self.checkBox)
-        self.checkBox_2 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox_2.setObjectName(_fromUtf8("checkBox_2"))
-        self.verticalLayout.addWidget(self.checkBox_2)
-        self.checkBox_3 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox_3.setObjectName(_fromUtf8("checkBox_3"))
-        self.verticalLayout.addWidget(self.checkBox_3)
-        self.checkBox_4 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox_4.setObjectName(_fromUtf8("checkBox_4"))
-        self.verticalLayout.addWidget(self.checkBox_4)
-        self.checkBox_5 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox_5.setObjectName(_fromUtf8("checkBox_5"))
-        self.verticalLayout.addWidget(self.checkBox_5)
-        self.comboBox = QtGui.QComboBox(self.centralwidget)
-        self.comboBox.setGeometry(QtCore.QRect(60, 130, 91, 21))
-        self.comboBox.setObjectName(_fromUtf8("comboBox"))
-        self.treeView = QtGui.QTreeView(self.centralwidget)
-        self.treeView.setGeometry(QtCore.QRect(200, 390, 256, 192))
-        self.treeView.setObjectName(_fromUtf8("treeView"))
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 992, 21))
-        self.menubar.setObjectName(_fromUtf8("menubar"))
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtGui.QStatusBar(MainWindow)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("MainWindow", "Verkefni 3", None))
-        self.checkBox.setText(_translate("MainWindow", "CheckBox", None))
-        self.checkBox_2.setText(_translate("MainWindow", "CheckBox", None))
-        self.checkBox_3.setText(_translate("MainWindow", "CheckBox", None))
-        self.checkBox_4.setText(_translate("MainWindow", "CheckBox", None))
-        self.checkBox_5.setText(_translate("MainWindow", "CheckBox", None))
-
-from pyqtgraph import PlotWidget
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'window.ui'
-#
-# Created: Sat Dec 13 17:13:37 2014
-#      by: PyQt4 UI code generator 4.11.3
-#
-# WARNING! All changes made in this file will be lost!
-
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(992, 655)
-        self.centralwidget = QtGui.QWidget(MainWindow)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.graphicsView = PlotWidget(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(200, 60, 731, 311))
-        self.graphicsView.setObjectName(_fromUtf8("graphicsView"))
-        self.verticalLayoutWidget = QtGui.QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(50, 160, 111, 111))
-        self.verticalLayoutWidget.setObjectName(_fromUtf8("verticalLayoutWidget"))
-        self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setMargin(0)
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.checkBox = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox.setObjectName(_fromUtf8("checkBox"))
-        self.verticalLayout.addWidget(self.checkBox)
-        self.checkBox_2 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox_2.setObjectName(_fromUtf8("checkBox_2"))
-        self.verticalLayout.addWidget(self.checkBox_2)
-        self.checkBox_3 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox_3.setObjectName(_fromUtf8("checkBox_3"))
-        self.verticalLayout.addWidget(self.checkBox_3)
-        self.checkBox_4 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox_4.setObjectName(_fromUtf8("checkBox_4"))
-        self.verticalLayout.addWidget(self.checkBox_4)
-        self.checkBox_5 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox_5.setObjectName(_fromUtf8("checkBox_5"))
-        self.verticalLayout.addWidget(self.checkBox_5)
-        self.comboBox = QtGui.QComboBox(self.centralwidget)
-        self.comboBox.setGeometry(QtCore.QRect(60, 130, 91, 21))
-        self.comboBox.setObjectName(_fromUtf8("comboBox"))
-        self.treeView = QtGui.QTreeView(self.centralwidget)
-        self.treeView.setGeometry(QtCore.QRect(510, 390, 256, 192))
-        self.treeView.setObjectName(_fromUtf8("treeView"))
-        self.listView = QtGui.QListView(self.centralwidget)
-        self.listView.setGeometry(QtCore.QRect(50, 390, 256, 192))
+        self.CountryBox = QtGui.QComboBox(self.verticalLayoutWidget)
+        self.CountryBox.setObjectName(_fromUtf8("CountryBox"))
+        self.verticalLayout.addWidget(self.CountryBox)
+        self.listView = QtGui.QListView(self.verticalLayoutWidget)
+        self.listView.setAutoFillBackground(False)
         self.listView.setObjectName(_fromUtf8("listView"))
+        self.verticalLayout.addWidget(self.listView)
+        self.Plot = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.Plot.setObjectName(_fromUtf8("Plot"))
+        self.verticalLayout.addWidget(self.Plot)
+        self.ClearPlot = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.ClearPlot.setObjectName(_fromUtf8("ClearPlot"))
+        self.verticalLayout.addWidget(self.ClearPlot)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 992, 21))
@@ -366,10 +65,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "Verkefni 3", None))
-        self.checkBox.setText(_translate("MainWindow", "CheckBox", None))
-        self.checkBox_2.setText(_translate("MainWindow", "CheckBox", None))
-        self.checkBox_3.setText(_translate("MainWindow", "CheckBox", None))
-        self.checkBox_4.setText(_translate("MainWindow", "CheckBox", None))
-        self.checkBox_5.setText(_translate("MainWindow", "CheckBox", None))
+        self.Plot.setText(_translate("MainWindow", "Plot", None))
+        self.ClearPlot.setText(_translate("MainWindow", "Clear Plot", None))
 
 from pyqtgraph import PlotWidget
