e975fafca7ef010274c9ba8dbd3a98ef7e423daf
Halldór Stefánsson <halldor90@gmail.com>
2014-12-14 20:58:45 +0000

    murf

--- Verkefni3/run.py
+++ Verkefni3/run.py
@@ -4,9 +4,9 @@ import theUI
 from manage_db import manage_db
 from import_data import import_data
 
-
 if __name__ == '__main__':
     mydb = manage_db('localhost','verkefni3', 'postgres', 'postgres')
     if mydb.missingData():
         data = import_data(mydb)
     window = theUI.loadUI(mydb)
+    
